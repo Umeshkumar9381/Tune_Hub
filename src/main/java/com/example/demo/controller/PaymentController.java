cbc2ce6de93feb88652813b71e6a3f5a094938d8
Umeshkumar9381 <umesh.c9381@gmail.com>
2024-01-12 15:39:01 +0530

    1st version of tunehub

---
+++ src/main/java/com/example/demo/controller/PaymentController.java
@@ -0,0 +1,67 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.entities.Song;
+import com.example.demo.entities.Users;
+import com.example.demo.services.UsersService;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class PaymentController {
+	
+	UsersService service;
+	@GetMapping("/pay")
+	public String pay() {
+		return "pay";
+
+		
+	}
+	@SuppressWarnings("finally")
+	@PostMapping("/createOrder")
+	@ResponseBody
+	public String createOrder(HttpSession session) {
+
+		int  amount  = 5000;
+		Order order=null;
+		try {
+			RazorpayClient razorpay=new RazorpayClient("rzp_test_qjYSuf1JG2dKT5", "vD3JSP7JyLkYFdK7y9fjQPYl");
+
+			JSONObject orderRequest = new JSONObject();
+			orderRequest.put("amount", amount*100); // amount in the smallest currency unit
+			orderRequest.put("currency", "INR");
+			orderRequest.put("receipt", "order_rcptid_11");
+
+			order = razorpay.orders.create(orderRequest);
+
+			String mail =  (String) session.getAttribute("email");
+
+			Users u = service.getUser(mail);
+			u.setPremium(true);
+			service.updateUser(u);
+
+		} catch (RazorpayException e) {
+			e.printStackTrace();
+		}
+		finally {
+			return order.toString();
+		}
+	}
+
+}
